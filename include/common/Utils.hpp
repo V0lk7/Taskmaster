bdd032ba9831648fc71ae5bf4e5aa8c808e0faef
jduval <jduval@student.42angouleme.fr>
2025-04-18 17:47:04 +0200

    [BUG] modify CmakeList to add symlink for lib + utils file

---
+++ include/common/Utils.hpp
@@ -0,0 +1,23 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+
+#include "pch.hpp"
+
+class Utils {
+public:
+  static std::vector<std::string> split(std::string const &input,
+                                        std::string const &delimiter);
+
+  static void trim(std::string &s);
+  static void rtrim(std::string &s);
+  static void ltrim(std::string &s);
+
+private:
+  Utils() = delete;
+  Utils(Utils &) = delete;
+  Utils(Utils &&) = delete;
+  Utils &operator=(Utils &) = delete;
+  Utils &operator=(Utils &&) = delete;
+};
+
+#endif
--- source/client/main.cpp
+++ source/client/main.cpp
@@ -1,6 +1,22 @@
+#include "nanomsg/nn.h"
+#include "nanomsg/pipeline.h"
+#include "yaml-cpp/yaml.h"
 #include <iostream>
+#include <unistd.h>
 
 int main(void) {
   std::cout << "TEST TEST" << std::endl;
+  int sock = nn_socket(AF_SP, NN_PULL);
+
+  if (sock > 0) {
+    close(sock);
+  }
+
+  try {
+    YAML::LoadAllFromFile("");
+  } catch (...) {
+    std::cout << "osef" << std::endl;
+  }
+
   return 0;
 }
