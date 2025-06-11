eb4eb1e80ad952fc4c0d09efe08d60f6cb4753d8
jduval <jduval@student.42angouleme.fr>
2025-06-11 08:50:58 +0200

    Modify cmake to make it pass the unitary test

--- test/daemon/parsing_conf_test.cpp
+++ test/daemon/parsing_conf_test.cpp
@@ -1,43 +1,44 @@
 #include "catch2/catch_test_macros.hpp"
 
 #include "daemon/parsing/parsing.hpp"
+#include <fcntl.h>
+#include <unistd.h>
 
 int parsingTest(std::string config_file) {
   if (config_file.empty()) {
-	return 1;
+    return 1;
   }
   if (access(config_file.c_str(), R_OK) == -1) {
-	return 1;
+    return 1;
   }
   try {
     Daemon *daemon = parsingFile(config_file);
-	delete daemon;
-  }
-  catch (const std::exception &e) {
-	std::cerr << "Error: " << e.what() << std::endl;
-	return 1;
+    delete daemon;
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
   }
   return 0;
 }
 
 TEST_CASE("Parsing conf.yaml", "[parsing]") {
-	REQUIRE(parsingTest("../../confs/conf.yaml") == 0);
+  REQUIRE(parsingTest("../../confs/conf.yaml") == 0);
 }
 TEST_CASE("Parsing conf_minimal.yaml", "[parsing]") {
-	REQUIRE(parsingTest("../../confs/conf_minimal.yaml") == 0);
+  REQUIRE(parsingTest("../../confs/conf_minimal.yaml") == 0);
 }
 TEST_CASE("Parsing conf_noprog.yaml", "[parsing]") {
-	REQUIRE(parsingTest("../../confs/conf_noprog.yaml") == 0);
+  REQUIRE(parsingTest("../../confs/conf_noprog.yaml") == 0);
 }
 TEST_CASE("Parsing conf_err_empty.yaml", "[parsing]") {
-	REQUIRE(parsingTest("../../confs/conf_err_empty.yaml") == 1);
+  REQUIRE(parsingTest("../../confs/conf_err_empty.yaml") == 1);
 }
 TEST_CASE("Parsing conf_err_noperms.yaml", "[parsing]") {
-	REQUIRE(parsingTest("../../confs/conf_err_noperms.yaml") == 1);
+  open("../../confs/conf_err_noperms.yaml", O_CREAT, 000);
+  REQUIRE(parsingTest("../../confs/conf_err_noperms.yaml") == 1);
+  unlink("../../confs/conf_err_noperms.yaml");
 }
 TEST_CASE("Parsing conf_err_incomplete_prog.yaml", "[parsing]") {
-	REQUIRE(parsingTest("../../confs/conf_err_incomplete_prog.yaml") == 1);
+  REQUIRE(parsingTest("../../confs/conf_err_incomplete_prog.yaml") == 1);
 }
-TEST_CASE("Empty parameter", "[parsing]") {
-	REQUIRE(parsingTest("") == 1);
-}
+TEST_CASE("Empty parameter", "[parsing]") { REQUIRE(parsingTest("") == 1); }
